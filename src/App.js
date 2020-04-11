9418bd07236a1e7dc3ef5f509df08d6648cce0c1
anigulkanyan <ani.gulkanyan@picsart.com>
2020-04-11 16:43:36 +0400

    Added some files.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Info from "./Components/info";
+import Center from './Components/pageCenter'
+import './App.css'
+function App() {
+  return (
+      <div className='mainContainer'>
+          <Info/>
+          <Center/>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/Components/info.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+    return (
+        <div className='leftPart'>
+            <p>Left part</p>
+            <p>Know more about weather in your town!</p>
+        </div>
+    )
+}
+
+export default Info;
